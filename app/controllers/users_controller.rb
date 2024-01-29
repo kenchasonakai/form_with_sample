b1aaa060f6784f268d4137112223941738d3599e
kenchaso <kenchasonakai@gmail.com>
2024-01-29 18:39:59 +0900

    add: user作成機能を追加

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,22 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      auto_login(@user)
+      redirect_to root_path, success: 'Your account has been created.'
+    else
+      flash.now[:danger] = 'There was a problem creating your account.'
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password)
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
   authenticates_with_sorcery!
+
+  validates :email, uniqueness: true, length: { minimum: 3 }
+  validates :password, length: { minimum: 3 }, if: -> { new_record? || changes[:crypted_password] }
 end
